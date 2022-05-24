c14f09a3ab14dd4296da06fe30f730dd68cc14db
Your Name <you@example.com>
2022-05-24 12:37:08 -0700

    created get request to get all from todo table

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get("/api/todo", async (req, res) => {
+    try {
+        await db.connect();
+        const result = await db.query("SELECT * FROM todos")
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on Port ${process.env.PORT}`);
