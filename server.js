87bc4ece6da9ffff8aad953331bc3a089fb6dae7
Your Name <you@example.com>
2022-05-24 11:43:34 -0700

    Testing basic funtionality of server.js

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+
+app.listen(process.env.port, () => {
+    console.log(`Listening on Port ${process.env.PORT}`);
+})
